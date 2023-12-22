cffa88a9e24f847904ec3098f2c64bd506ff349c
Yioux37 <114407163+Yioux37@users.noreply.github.com>
2023-12-22 14:47:17 +0100

    front login et register

--- src/screens/loginPage.js
+++ src/screens/loginPage.js
@@ -1,81 +1,154 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import firebase from '../services/firebaseConfig';
-import { auth } from '../services/firebaseConfig';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    ActivityIndicator,
+    ImageBackground,
+    Image // Import the Image component
+} from 'react-native';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../services/firebaseConfig';
 
 const LoginPage = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [imageLoaded, setImageLoaded] = useState(false);
 
     const handleLogin = async () => {
         if (!email || !password) {
-          Alert.alert("Erreur", "Veuillez remplir tous les champs.");
-          return;
+            Alert.alert("Erreur", "Veuillez remplir tous les champs.");
+            return;
         }
-      
+
         try {
-          setIsLoading(true);
-          await signInWithEmailAndPassword(auth, email, password);
-          // Redirection après connexion réussie
-          navigation.navigate('Home');
+            setIsLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+            navigation.navigate('Main'); // Redirection après connexion réussie
         } catch (error) {
-          Alert.alert("Erreur de Connexion", error.message);
+            Alert.alert("Erreur de Connexion", error.message);
         } finally {
-          setIsLoading(false);
+            setIsLoading(false);
         }
-    };      
+    };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Connexion</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Mot de passe"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-            <Button 
-                title={isLoading ? 'Connexion...' : 'Se Connecter'}
-                onPress={handleLogin}
-                disabled={isLoading}
-            />
-            <Button 
-                title="Créer un compte"
-                onPress={() => navigation.navigate('Register')}
-            />
-        </View>
+        <ImageBackground
+            source={require('../../assets/overlay.png')} // Your image background
+            style={styles.backgroundImage}
+            onLoadEnd={() => setImageLoaded(true)}
+        >
+            {imageLoaded ? (
+                <View style={styles.container}>
+                    <Image
+                        source={require('../../assets/logo.png')} // Replace with your logo path
+                        style={styles.logo}
+                    />
+                    <Text style={styles.title}>Connexion</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Votre mail"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Votre mot de passe"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                    />
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={handleLogin}
+                        disabled={isLoading}
+                    >
+                        {isLoading ? (
+                            <ActivityIndicator size="small" color="#FFF" />
+                        ) : (
+                            <Text style={styles.buttonText}>Se Connecter</Text>
+                        )}
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.linkButton}
+                        onPress={() => navigation.navigate('Register')}
+                    >
+                        <Text style={styles.linkText}>Pas encore de compte?Créer un compte</Text>
+                    </TouchableOpacity>
+                </View>
+            ) : (
+                <ActivityIndicator size={'large'} color={'#0339C5'} />
+            )}
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
+    backgroundImage: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    },
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-        padding: 20,
+        justifyContent: 'flex-start', 
+        paddingTop: 135, 
+        paddingHorizontal: 20, 
+    },
+    logo: {
+        width: 120, 
+        height: 120,
+        marginBottom: 30, 
     },
     title: {
         fontSize: 24,
+        color: '#fff',
         marginBottom: 20,
     },
     input: {
         width: '100%',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        padding: 10,
-        borderRadius: 5,
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
         marginBottom: 10,
+        fontSize: 16,
+    },
+    button: {
+        width: '100%',
+        padding: 15,
+        backgroundColor: '#fff', 
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 60, 
+        marginBottom: 1, 
+    },
+    buttonText: {
+        color: '#000000', 
+        fontSize: 16,
+        fontWeight: 'light',
+    },
+    linkButton: {
+        position: 'absolute',
+        bottom: 20,
+        width: '100%',
+        alignItems: 'center',
+        padding: 10 ,
+    },
+    linkText: {
+        color: '#fff',
+        textDecorationLine: 'underline',
+        bottom: 5, 
+        padding: 20 ,
+        textAlign: 'center',
+
     },
 });
 
--- src/screens/registerPage.js
+++ src/screens/registerPage.js
@@ -1,116 +1,159 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    ActivityIndicator,
+    ImageBackground,
+    Image // Import the Image component
+} from 'react-native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../services/firebaseConfig';
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import { useUserContext } from '../contexts/userContext';
-import { Timestamp } from 'firebase/firestore';
 
 const RegisterPage = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const { setUserId } = useUserContext();
-    const firestore = getFirestore(); // Get the Firestore instance
+    const [imageLoaded, setImageLoaded] = useState(false);
 
     const handleSignUp = async () => {
         if (!email || !password) {
-            Alert.alert("Error", "Email and password must not be empty");
+            Alert.alert("Erreur", "Veuillez remplir tous les champs.");
             return;
-          }
-
+        }
         setIsLoading(true);
-    
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const userId = userCredential.user.uid;
-    
-            // Use Firebase's Timestamp for the current time
-            const creationTime = Timestamp.now();
-    
-            // Create a document in Firestore in the "users" collection with the userId
-            await setDoc(doc(firestore, "users", userId), {
-                email: email,
-                firstName: "",
-                lastName: "",
-                age: "",
-                currentClass: "",
-                specialty: "",
-                school: "",
-                creationTime: creationTime 
-            });
-    
-            // Set the user ID in the context here
-            setUserId(userId);
-    
-            // Navigate to the 'AdditionalInfoScreen' screen
+            // Navigate to the next screen or perform additional setup
             navigation.reset({
                 index: 0,
-                routes: [{ name: 'AdditionalInfo' }],
+                routes: [{ name: 'AdditionalInfo' }], // Replace 'Form' with the actual route you wish to navigate to
             });
-    
-            // Prevent further actions until navigation is complete
-            return;
         } catch (error) {
-            Alert.alert("Error", error.message);
+            Alert.alert("Erreur", error.message);
         } finally {
-            setIsLoading(false); // Ensure loading is false in case of success or error
+            setIsLoading(false);
         }
-    };    
+    };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Créer un compte</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Mot de passe"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-            <Button 
-                title={isLoading ? 'Création du compte...' : 'Créer un compte'}
-                onPress={handleSignUp}
-                disabled={isLoading}
-            />
-            <Button 
-                title="Se connecter"
-                onPress={() => navigation.navigate('Login')} 
-            />
-        </View>
+        <ImageBackground
+            source={require('../../assets/overlay.png')} // Replace with your desired background image
+            style={styles.backgroundImage}
+            onLoadEnd={() => setImageLoaded(true)}
+        >
+            {imageLoaded ? (
+                <View style={styles.container}>
+                    <Image
+                        source={require('../../assets/logo.png')} // Replace with your logo image path
+                        style={styles.logo}
+                    />
+                    <Text style={styles.title}>Créer un compte</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Votre mail"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Votre mot de passe"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                    />
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={handleSignUp}
+                        disabled={isLoading}
+                    >
+                        {isLoading ? (
+                            <ActivityIndicator size="small" color="#FFF" />
+                        ) : (
+                            <Text style={styles.buttonText}>S’inscrire</Text>
+                        )}
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.linkButton}
+                        onPress={() => navigation.navigate('Login')} // Make sure 'Login' is your login screen's route name
+                    >
+                        <Text style={styles.linkText}>Déjà inscrit ? Se Connecter</Text>
+                    </TouchableOpacity>
+                </View>
+            ) : (
+                <ActivityIndicator
+                    size={'large'}
+                    color={'#0339C5'} />
+            )}
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
+    backgroundImage: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    },
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-        padding: 20,
+        justifyContent: 'flex-start', 
+        paddingTop: 135, 
+        paddingHorizontal: 20, 
+    },
+    logo: {
+        width: 120, 
+        height: 120, 
+        marginBottom: 30, 
     },
     title: {
         fontSize: 24,
+        color: '#fff',
         marginBottom: 20,
     },
     input: {
         width: '100%',
-        borderWidth: 1,
-        borderColor: '#ddd',
-        padding: 10,
-        borderRadius: 5,
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
         marginBottom: 10,
+        fontSize: 16,
+    },
+    button: {
+        width: '100%',
+        padding: 15,
+        backgroundColor: '#fff', 
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 60, 
+        marginBottom: 1, 
+    },
+    buttonText: {
+        color: '#000000',
+        fontSize: 16,
+        fontWeight: 'light',
+    },
+    linkButton: {
+        position: 'absolute',
+        bottom: 20,
+        width: '100%',
+        alignItems: 'center',
+        padding: 10 
+    },
+    linkText: {
+        color: '#fff',
+        textDecorationLine: 'underline',
+        position: 'absolute',
+        bottom: 5, 
+        padding: 20 
     },
 });
-
 export default RegisterPage;
-
-
-
